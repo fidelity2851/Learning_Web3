5ffaab91a4354722bc668ea1e0cdf6e6cc4f274d
Ukwuani Fidelis <ukwuanifidelis2851@gmail.com>
2022-11-02 21:27:28 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <ConnectWallet/>
+</template>
+
+<!-- JS -->
+<script setup>
+import ConnectWallet from './components/ConnectWallet.vue';
+
+</script>
+
+<!-- CSS -->
+<style scoped>
+
+</style>
